e204ecb721fbfd74ae005a95e3a587d1a97d7b4a
Sohum Chandra Srivastava <sohum611@gmail.com>
2024-01-25 23:22:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ const quoteData = [
       "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.",
     author: "Steve Jobs"
   },
-  { text: "hey pretty!", author: "Pratu" },
+  { text:"hey pretty!", author: "Pratu" },
   {
     text:
       "My mama always said, life is like a box of chocolates. You never know what you're gonna get.",
