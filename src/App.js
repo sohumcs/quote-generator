59d8f83d9876eab21e2b670b9dc8dd3dd11000bb
Sohum Chandra Srivastava <sohum611@gmail.com>
2024-01-27 01:38:01 +0530

    com

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ const quoteData = [
   {
     text:
       "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.",
-    author: "Steve Job"
+    author: "Steve Jobs"
   },
   { text:"hey pretty!", author: "Pratu" },
   {
